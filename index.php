e98df71d9110b81dfe61838e5748e1749e66b775
me_media <vgoogle@i.ua>
2018-10-28 23:27:14 +0200

    Me\Logger - Vendor

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$logger = new Vendor\Me\Logger\Logger();
+$logger = new Me\Logger\Logger();
 
-$strategy = new Vendor\Me\Logger\Base\FileAndMailLogger(__DIR__ . './log.log', 'support@site.com');
+$strategy = new Me\Logger\Base\FileAndMailLogger(__DIR__ . './log.log', 'support@site.com');
 
-$strategy_to_email = new Vendor\Me\Logger\Base\MailLogger('support@site.com');
-$strategy_to_file = new Vendor\Me\Logger\Base\FileLogger('./log.log');
+$strategy_to_email = new Me\Logger\Base\MailLogger('support@site.com');
+$strategy_to_file = new Me\Logger\Base\FileLogger('./log.log');
 
 $logger->writeLog('Error in OOP', $strategy);
