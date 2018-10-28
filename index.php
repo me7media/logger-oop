37ee33e7b28363cead55ebeb7e55e9b4127037f3
me_media <vgoogle@i.ua>
2018-10-28 23:58:20 +0200

    Good

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $logger = new Me\Logger\Logger();
 
 $strategy = new Me\Logger\Base\FileAndMailLogger(__DIR__ . './log.log', 'support@site.com');
 
-$strategy_to_email = new Me\Logger\Base\MailLogger('support@site.com');
+$strategy_to_email = new Me\Logger\Base\MailLogger('support@mail.com');
 $strategy_to_file = new Me\Logger\Base\FileLogger('./log.log');
 
 $logger->writeLog('Error in OOP', $strategy);
